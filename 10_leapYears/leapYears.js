427ce255b420f117411ac175507a48eeb3aad15c
Oxwillfollow <240659154+Oxwillfollow@users.noreply.github.com>
2025-11-15 17:19:28 +0200

    Solve 10_LEAPYEARS excercise

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // every leap year is divisible by 4
+    if(year % 4 !== 0) return false;
 
+    // if a year is divisible by 100 it's not a leap year, unless it's also divisible by 400
+    if(year % 100 == 0 && year % 400 != 0) return false;
+
+    return true;
 };
 
 // Do not edit below this line
