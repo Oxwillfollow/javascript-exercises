c0570fb4412280a61f696aed6c9e2d945eb4f36d
Oxwillfollow <240659154+Oxwillfollow@users.noreply.github.com>
2025-11-15 16:47:44 +0200

    Solve 09_SUMALL excercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    // non-int should return error msg
+    if(!Number.isInteger(int1) || !Number.isInteger(int2))
+        return 'ERROR';
 
+    // negative int should return error msg
+    if(int1 < 0 || int2 < 0)
+        return 'ERROR';
+
+    const smallerInt = int1 < int2 ? int1 : int2;
+    const largerInt = int1 > int2 ? int1 : int2;
+    var sum = 0;
+
+    for (let index = smallerInt; index <= largerInt; index++) {
+        sum+= index;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
