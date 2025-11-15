da875964f4b5ed48cef678b13338a1fe296a2fd7
Oxwillfollow <240659154+Oxwillfollow@users.noreply.github.com>
2025-11-15 16:12:17 +0200

    Solve 08_REMOVEFROMARRAY

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    if(args.length > 0){
+        for (let i = arr.length-1; i >= 0; i--) {
+            if(args.includes(arr[i])) arr.splice(i,1);
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
