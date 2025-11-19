a58396775326da7487a2fc219ba938697727acb9
Oxwillfollow <240659154+Oxwillfollow@users.noreply.github.com>
2025-11-19 10:25:15 +0200

    Solve 16_FINDTHEOLDEST excercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce((acc, currentVal) => {
+        let currentAge = currentVal.yearOfDeath ? currentVal.yearOfDeath - currentVal.yearOfBirth : new Date().getFullYear() - currentVal.yearOfBirth;
+        let accAge = acc.yearOfDeath ? acc.yearOfDeath - acc.yearOfBirth : new Date().getFullYear() - acc.yearOfBirth;
+        
+        if(accAge < currentAge) acc = currentVal;
 
+        return acc;
+    });
 };
 
 // Do not edit below this line
