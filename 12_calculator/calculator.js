21147c4e3511a1e78bc884b5d8e4cba46f41a536
Oxwillfollow <240659154+Oxwillfollow@users.noreply.github.com>
2025-11-19 09:00:02 +0200

    Solve 12_CALCULATOR excercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbersArr) {
+	return numbersArr.reduce((acc, currentVal) => acc + currentVal, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbersArr) {
+	return numbersArr.reduce((acc, currentVal) => acc * currentVal, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base,exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+
+  if(num > 0){
+    for (let index = 1; index < num; index++) {
+      result = result*index;
+    }
+    return result*num;
+  }
+  else {
+    return result;
+  }
 };
 
 // Do not edit below this line
