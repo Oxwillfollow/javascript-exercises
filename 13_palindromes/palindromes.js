42f68103feca37c5eb7c86e7872ab53ad81fb1d7
Oxwillfollow <240659154+Oxwillfollow@users.noreply.github.com>
2025-11-19 09:30:47 +0200

    Solve 13_PALINDROMES excercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    // remove whitespace, punctuation and ignore casing
+    str = str.replace(/\s|[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '').toLowerCase();
+    return str === str.split('').reverse().join('');
 };
 
 // Do not edit below this line
