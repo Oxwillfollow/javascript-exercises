af9acc3e7fb40646cc24b7ed857aef56cbdb1ca5
Oxwillfollow <240659154+Oxwillfollow@users.noreply.github.com>
2026-02-12 00:35:53 +0200

    Solve 2-contains excercise

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,18 @@
-const contains = function() {
-  
+const contains = function (obj, item) {
+  // stop if item is found
+  if (obj === item) return true;
+  if (isNaN(obj) && isNaN(item) && typeof item === "number") return true;
+
+  // stop at non-objects or empty objects
+  if (typeof obj !== "object" || obj === null) return;
+  if (Object.keys(obj).length === 0) return;
+
+  for (let val of Object.values(obj)) {
+    if (contains(val, item) == true) return true;
+  }
+
+  return false;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
