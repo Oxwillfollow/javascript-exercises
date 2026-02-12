53a762924936c563b9db617245a022db9ce257e4
Oxwillfollow <240659154+Oxwillfollow@users.noreply.github.com>
2026-02-12 14:17:48 +0200

    Solve 3-totalintegers excercise

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,24 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (obj, isFirstCall = true) {
+  let total = 0;
+
+  if (Array.isArray(obj)) {
+    for (let i = 0; i < obj.length; i++) {
+      total += totalIntegers(obj[i], false);
+    }
+  } else if (typeof obj === "object" && obj !== null) {
+    for (let val of Object.values(obj)) {
+      total += totalIntegers(val, false);
+    }
+  } else {
+    if (isFirstCall) return undefined;
+
+    if (Number.isInteger(obj)) {
+      return 1;
+    }
+  }
+
+  return total;
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
