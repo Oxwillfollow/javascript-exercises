ab18db5a41010f840159b0ea84aa207faa78c946
Oxwillfollow <240659154+Oxwillfollow@users.noreply.github.com>
2026-02-12 22:28:40 +0200

    Solve 5-pascals excercise

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,24 @@
-const pascal = function() {
-  
+const pascal = function (n, result = [1], index = 0) {
+  if (n <= 0) return;
+
+  if (index >= n - 1) return result;
+
+  // add zeroes to beginning and end of current pascal's row
+  let arrPrep = [0].concat(result).concat(0);
+
+  // reset the current row
+  let currentRow = [];
+
+  // add the numbers together and push to array
+  for (let j = 0; j < arrPrep.length; j++) {
+    if (arrPrep[j + 1] !== undefined) {
+      let sum = arrPrep[j] + arrPrep[j + 1];
+      currentRow.push(sum);
+    }
+  }
+
+  return pascal(n, currentRow, index + 1);
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
