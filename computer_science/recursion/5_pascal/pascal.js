5418f88ea0917995cdcb462881b6611dfcac07b5
Oxwillfollow <240659154+Oxwillfollow@users.noreply.github.com>
2026-02-13 13:48:23 +0200

    Rework the solution to be simpler and cleaner

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,15 +1,12 @@
-const pascal = function (n, result = [1], index = 0) {
-  if (n <= 0) return;
-
-  if (index >= n - 1) return result;
+const pascal = function (n) {
+  if (n === 1) return [1];
 
   // add zeroes to beginning and end of current pascal's row
-  let arrPrep = [0].concat(result).concat(0);
+  let arrPrep = [0].concat(pascal(n - 1)).concat(0);
 
-  // reset the current row
   let currentRow = [];
 
-  // add the numbers together and push to array
+  // add the numbers together
   for (let j = 0; j < arrPrep.length; j++) {
     if (arrPrep[j + 1] !== undefined) {
       let sum = arrPrep[j] + arrPrep[j + 1];
@@ -17,7 +14,7 @@ const pascal = function (n, result = [1], index = 0) {
     }
   }
 
-  return pascal(n, currentRow, index + 1);
+  return currentRow;
 };
 
 // Do not edit below this line
